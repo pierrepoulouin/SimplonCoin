e8776fd4c2f41c06b3191f5e921d46e7deb0f277
DJ Logan <pierrepoulouin@gmail.com>
2018-07-31 19:29:16 +0200

    First commit (ajout de la class Comptes)

---
+++ SimplonCoin/src/banque/Comptes.java
@@ -0,0 +1,33 @@
+package banque;
+
+public class Comptes {
+	public int ID = 0;
+	public String nom;
+	public String prenom;
+	public double solde = 500.00;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public double getSolde() {
+		return solde;
+	}
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	
+}
